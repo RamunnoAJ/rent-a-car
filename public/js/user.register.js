c9dec9586c885b27f7789020382c050ee2c0c65c
RamunnoAJ <ramunnoagustin@gmail.com>
2024-07-25 15:36:32 -0300

    feat: add register to the controller

---
+++ public/js/user.register.js
@@ -0,0 +1,113 @@
+const $form = document.querySelector("form");
+
+$form.addEventListener("submit", handleSubmit);
+
+/** @param {SubmitEvent} e  */
+async function handleSubmit(e) {
+    const $errorContainer = document.querySelector("#errors-container");
+    const $errors = document.querySelector("#errors");
+    $errors.innerHTML = "";
+    if (!$errorContainer.className.includes("is-hidden")) {
+        $errorContainer.classList.add("is-hidden");
+    }
+
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const repeatPassword = e.target["repeat-password"].value;
+    const phone = e.target.phone.value;
+    const country = e.target.country.value;
+    const address = e.target.address.value;
+    const driverLicense = e.target["driver-license"].value;
+
+    const isValid = validateForm({
+        name,
+        email,
+        password,
+        repeatPassword,
+        phone,
+        country,
+        address,
+        driverLicense
+    });
+
+    if (!isValid) {
+        return;
+    }
+}
+
+/**
+ * @param {object} formData
+ * @returns {boolean}
+ */
+function validateForm(formData) {
+    const {
+        name,
+        email,
+        password,
+        repeatPassword,
+        phone,
+        country,
+        address,
+        driverLicense
+    } = formData;
+
+    const errors = [];
+
+    if (!name) {
+        errors.push("Name is required.");
+    }
+
+    if (!email) {
+        errors.push("Email is required.");
+    }
+
+    if (!password) {
+        errors.push("Password is required.");
+    }
+
+    if (!repeatPassword) {
+        errors.push("Repeat password is required.");
+    }
+
+    if (!phone) {
+        errors.push("Phone is required.");
+    }
+
+    if (!country) {
+        errors.push("Country is required.");
+    }
+
+    if (!address) {
+        errors.push("Address is required.");
+    }
+
+    if (!driverLicense) {
+        errors.push("Driver license is required.");
+    }
+
+    if (password !== repeatPassword) {
+        errors.push("Passwords not matching");
+    }
+
+    if (errors.length > 0) {
+        showErrors(errors);
+        return true;
+    }
+
+    return false;
+}
+
+/** @param {Array<string>} errors  */
+function showErrors(errors) {
+    const $errorContainer = document.querySelector("#errors-container");
+    const $errors = document.querySelector("#errors");
+
+    $errorContainer.classList.remove("is-hidden");
+
+    errors.forEach(e => {
+        const $error = document.createElement("li");
+        $error.textContent = e;
+        $errors.appendChild($error);
+    });
+}
